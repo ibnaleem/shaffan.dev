e3541356b0c0055436bc1cb11a99e07ebc98d0a3
ibnaleem <ibnaleem@outlook.com>
2025-12-19 19:06:41 +0000

    feat: integrate cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
