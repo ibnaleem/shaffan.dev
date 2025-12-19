372a43ed0991492b94c041d3e2e78c8d7eae9bf3
ibnaleem <ibnaleem@outlook.com>
2025-12-19 18:30:37 +0000

    feat: create api endpoint for downloading pgp keys

---
+++ src/pages/api/pgp.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+import { readFileSync } from "fs";
+
+export const GET: APIRoute = async () => {
+  const pgpKey = readFileSync("public/pgp.txt", "utf-8");
+
+  return new Response(pgpKey, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Content-Disposition": 'attachment; filename="shaffan-pgp.txt"',
+    },
+  })
+}
