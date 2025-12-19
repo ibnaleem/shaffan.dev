b3d143a0d3610dda87f431853853a086184e7ee2
ibnaleem <ibnaleem@outlook.com>
2025-12-19 18:02:02 +0000

    feat: query slug via slug api

---
+++ src/pages/api/posts/[slug].ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import prisma from "../../../lib/prisma"
+
+export const GET: APIRoute = async ({ params }) => {
+
+  const { slug } = params;
+
+  const post = await prisma.post.findFirst({
+    where: { slug: slug, published: true },
+    select: { id: true, title: true, content: true, slug: true, signature: true, createdAt: true }
+  })
+
+  return new Response(JSON.stringify(post), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
