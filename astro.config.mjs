0860bfe6812e1f7549bc555190380a85baedddd2
ibnaleem <ibnaleem@outlook.com>
2025-12-19 23:28:53 +0000

    configure node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
