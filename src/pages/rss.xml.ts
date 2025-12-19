a174d551f1a9aa43ac5d1329ac4a82753bc1d860
ibnaleem <ibnaleem@outlook.com>
2025-12-19 14:25:53 +0000

    feat: create rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import prisma from "../lib/prisma";
+
+export async function get() {
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    orderBy: {createdAt: "desc"},
+    select: {id: true, title: true, content: true, slug: true, signature: true, createdAt: true}
+  })
+
+  return rss({
+    title: "shaffan.dev - RSS Feed",
+    description: "Stay updated with the latest posts from shaffan.dev",
+    site: "https://shaffan.dev",
+    items: posts.map((post) => ({
+      title: post.title,
+      pubDate: post.createdAt,
+      description: post.signature ? `This post is PGP signed. Signature:\n\n${post.signature}` : "This post is not signed.",
+      link: `https://shaffan.dev/post/${post.slug}`,
+      content: post.content,
+    })),
+    stylesheet: "/rss/styles.xsl",
+  })
+}
